e86eecc3706ef075eb443f7be1a427bfd991292c
Zarrasis <matttaubler@knights.ucf.edu>
2017-06-16 19:28:57 -0400

    Exe and work

--- matrix.c
+++ matrix.c
@@ -2,18 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int determinant(){
+    return 0;
+}
+
+int getMatrix()
+{
+	printf("What is your matrix size? Enter two numbers n x m. \nFor example a 2x2 matrix can be entered as 2 2.");
+	int m = 0;
+	int n = 0;
+	scanf("%d, %d", n, m);
+	int matrixArray[n][m];
+
 
 int main()
 {
 
-	int choice = 0; 
-	printf("What would you like to do?\n1.Determinant")
-	printf("What is your matrix size? Enter two numbers n x m. For Example a 2x2 matrix can be entered as 2 2.")
-	int m = 0;
-	int n = 0; 
-	scanf("%d, %d", n, m);
+	int choice = 0;
+	printf("What would you like to do?\n1.Determinant\n\n");
+	switch(choice)
+	{
+		case 1:
+		{
+			getMatrix();
+			determinant();
+			break;
+		}
+		case 2:
+		{
+			printf("Not yet complete")
+		}
+	}
+
+	
 
 
-
-	return 0; 
+	return 0;
 }
+
