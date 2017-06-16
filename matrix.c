3ef59952c98ffd36999a50744ae5c7306b82993a
Zarrasis <matttaubler@knights.ucf.edu>
2017-06-16 19:19:05 -0400

    First commit

---
+++ matrix.c
@@ -0,0 +1,19 @@
+/// Matrix calculator
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+	int choice = 0; 
+	printf("What would you like to do?\n1.Determinant")
+	printf("What is your matrix size? Enter two numbers n x m. For Example a 2x2 matrix can be entered as 2 2.")
+	int m = 0;
+	int n = 0; 
+	scanf("%d, %d", n, m);
+
+
+
+	return 0; 
+}
