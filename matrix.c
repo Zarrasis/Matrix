8f820dbf90c57d05872a1bb569591f7bcd35b008
Zarrasis <matttaubler@knights.ucf.edu>
2017-08-27 18:43:56 -0400

    Add and sub

--- matrix.c
+++ matrix.c
@@ -3,21 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// n is size of current matrix, m is matrix, i is a counter for row expansion position
 int determinant(int n, int m[10][10], int i){
-	printf("%d", &m[0][0]);
+	//printf("%d", m[0][0]);
 	if(n==1)
 		return m[0][0];
 	if(n==2)
 		return m[0][0]*m[1][1] - m[1][0]*m[0][1];
-	if(i<n)
+	if(i < n)
 		return m[0][i]*reduceMatrix(m,i,0,n) + determinant(n,m,i+1);
+	return;
 }
 
 
 int reduceMatrix(int m[10][10], int col, int row, int n){
 	removeCol(m, col, n, n);
 	removeRow(m, row, n);
-	return m; 
+	return determinant(n-1,m,0); 
 }
 
 
@@ -59,12 +61,37 @@ void printMatrix(int n, int matrixArray[10][10]){
 	return;
 }
 
+int add(int m[10][10], int m2[10][10], int w, int h)
+{	
+	int i = 0, j = 0; 
+	for(i = 0; i < w; i++)
+	{
+		for(j = 0; j < h; j++)
+		{
+			m[i][j] += m2[i][j];
+		}
+	}
+	return m; 
+}
+
+int sub(int m[10][10], int m2[10][10], int w, int h)
+{	
+	int i = 0, j = 0; 
+	for(i = 0; i < w; i++)
+	{
+		for(j = 0; j < h; j++)
+		{
+			m[i][j] -= m2[i][j];
+		}
+	}
+	return m; 
+}
 
 void getMatrix(){
 
 	int n = 0, i = 0, j = 0, rowCount = 0;
 	int matrixArray[10][10] = {0};
-	int det = 0; 
+
 	char choice = "";
 
 	printf("What is your matrix size? Enter a number n x n.\nFor example a 3 x 3 matrix can be entered as 3.\n");
@@ -84,58 +111,46 @@ void getMatrix(){
 		printMatrix(n,matrixArray);
 	}
 
+	// TODO add check
 	printf("\nIs this matrix correct? Enter yes/no\n");
         printMatrix(n,matrixArray);
+
+    return m; 
     
-    printf("Whatever we are doing it anyways.\n");   
-   	det = determinant(n, matrixArray, 0);
-   	printf("%d", &det);
-
-    //scanf("%s",choice);
-
-   // if(choice == "yes")
-   //{
-    	//det = determinant(n,matrixArray,0);
-      //  printf("%d", &det);
-   // }
-   // else
-  //  {
-   //     printf("\nPlease re-enter Matrix\n");
-        //clear matrix
-  //  }
-
-	return;
 }
 
-void getRotMatrix(){
+// void getRotMatrix(){
 
-	int i = 0;
-	int matrix[3]
-	printf("Your matrix should be a 1x3 matrix, please enter it now. \n");
-	for(i = 0; i < 3; i++)
-		scanf("%d", &matrix[i]);
-	rotate(matrix);
-	printMatrix(3, matrix);
-}
+// 	int i = 0;
+// 	int matrix[3]
+// 	printf("Your matrix should be a 1x3 matrix, please enter it now. \n");
+// 	for(i = 0; i < 3; i++)
+// 		scanf("%d", &matrix[i]);
+// 	rotate(matrix);
+// 	printMatrix(3, matrix);
+// }
 
-void rotate(int** matrix){
+// void rotate(int** matrix){
 	
-	// inc
-	return;
-}
+// 	// inc
+// 	return;
+// }
 
 
 int main(){
-
+	int m[10][10] = {0};
+	int det = 0;
 	int choice = 0;
-	printf("What would you like to do?\n1.Determinant\n2.Rotation About Z for pi/4\n");
+	printf("What would you like to do?\n1.Determinant\n2.Rotation About Z for pi/4\n3.Add Matrix\n");
 	scanf("%d", &choice);
 
 	switch(choice)
 	{
 		case 1:
 		{
-			getMatrix();
+			m = getMatrix();
+			det = determinant(m);
+			printf("%d", det);
 			break;
 		}
 		case 2:
